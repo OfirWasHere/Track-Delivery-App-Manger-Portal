709d3217c5fa6ec23c60d3409bde102f6e360ede
OfirIsHere <Ofirdoron19@gmail.com>
2024-11-26 01:25:49 +0200

    added welcome section

--- src/Components/common/Navbar/Navbar.tsx
+++ src/Components/common/Navbar/Navbar.tsx
@@ -7,22 +7,107 @@ import {
   Toolbar,
   Button,
   Link,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  useMediaQuery,
+  useTheme,
 } from "@mui/material";
 import { NavLink } from "react-router-dom";
-import useIsMobile from "../../Hooks/useIsMobile";
 import RoutesNav from "../../Routes/RoutesNav";
-import BurgerMenuIcon from "../BurgerMenu/BurgerMenuIcon/BurgerMenuIcon";
 import LoginModal from "../LoginModal/LoginModal";
 import "./Navbar.css";
-import { Truck } from "lucide-react";
+import { Truck, Menu } from "lucide-react";
+import useIsMobile from "../../Hooks/useIsMobile";
 
-interface navbarProps {
+interface NavbarProps {
   onLoginClick: () => void;
 }
 
-function NavbarDesktop({ onLoginClick }: navbarProps): JSX.Element {
+function Navbar(): JSX.Element {
+  const isMobile = useIsMobile();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isBurgerMenuOpen, setIsBurgerMenuOpen] = useState(false);
+
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
+  const toggleBurgerMenu = () => setIsBurgerMenuOpen(!isBurgerMenuOpen);
+
+  const NavbarContent = (
+    <>
+      <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+        <Truck size={32} color="#2563EB" style={{ marginRight: 16 }} />
+        <Typography variant="h6" color="text.primary" fontWeight="bold">
+          Ofir Software
+        </Typography>
+      </Box>
+
+      {!isMobile && (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            flex: 1,
+            pr: 2,
+          }}
+        >
+          {RoutesNav.slice()
+            .reverse()
+            .map((route, index) => (
+              <Link
+                href={`#${route.toPath}`}
+                className="NavLinks"
+                key={index}
+                sx={{ textDecoration: "none", mx: "16px" }}
+              >
+                <Typography variant="h6">{route.routeName}</Typography>
+              </Link>
+            ))}
+        </Box>
+      )}
+
+      {isMobile ? (
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          onClick={toggleBurgerMenu}
+          sx={{
+            ml: 2,
+            color: "black",
+            bgcolor: "white",
+            "&:hover": {
+              bgcolor: "grey.200",
+            },
+          }}
+        >
+          <Menu />
+        </IconButton>
+      ) : (
+        <Box>
+          <Button
+            variant="contained"
+            size="large"
+            sx={{
+              bgcolor: "#212121",
+              color: "#fff",
+              "&:hover": {
+                bgcolor: "#424242",
+              },
+            }}
+            onClick={handleOpenModal}
+          >
+            <Typography variant="h6">התחברות</Typography>
+          </Button>
+        </Box>
+      )}
+    </>
+  );
+
   return (
-    <Box>
+    <Box sx={{ flexGrow: 1 }} display="block">
+      <LoginModal open={isModalOpen} onClose={handleCloseModal} />
       <AppBar
         sx={{
           bgcolor: "background.paper",
@@ -30,7 +115,6 @@ function NavbarDesktop({ onLoginClick }: navbarProps): JSX.Element {
           px: 4,
           justifyContent: "space-between",
           alignItems: "center",
-          position: "relative", // TODO: Remove this?
         }}
       >
         <Toolbar
@@ -41,72 +125,47 @@ function NavbarDesktop({ onLoginClick }: navbarProps): JSX.Element {
             alignItems: "center",
           }}
         >
-          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-            <Truck size={32} color="#2563EB" style={{ marginRight: 16 }} />
-            <Typography variant={"h6"} color={"text.primary"} fontWeight="bold">
-              TruckTrack
-            </Typography>
-          </Box>
-
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "flex-end",
-              flex: 1,
-              pr: 2,
-            }}
-          >
-            {RoutesNav.slice()
-              .reverse()
-              .map((route, index) => (
-                <Link
-                  href={`#${route.toPath}`}
-                  className={"NavLinks"}
-                  key={index}
-                  sx={{ textDecoration: "none", mx: "16px" }}
-                >
-                  <Typography variant="h5">{route.routeName}</Typography>
-                </Link>
-              ))}
-          </Box>
-
-          <Box>
-            <Button
-              variant="contained"
-              size="large"
-              sx={{ bgcolor: "#212121", color: "#fff" }}
-              onClick={onLoginClick}
-            >
-              <Typography variant="h6">התחברות</Typography>
-            </Button>
-          </Box>
+          {NavbarContent}
         </Toolbar>
       </AppBar>
-    </Box>
-  );
-}
-
-function Drawer({ onLoginClick }: navbarProps) {
-  return (
-    <Box position="absolute" right={"24px"} top={"24px"}>
-      <Box>
-        <BurgerMenuIcon />
-      </Box>
-    </Box>
-  );
-}
-
-function Navbar(): JSX.Element {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const isMobile = useIsMobile();
-  const handleOpenModal = () => setIsModalOpen(true);
-  const handleCloseModal = () => setIsModalOpen(false);
-
-  return (
-    <Box sx={{ flexGrow: 1 }} display={"block"}>
-      <LoginModal open={isModalOpen} onClose={handleCloseModal} />
-      {!isMobile && NavbarDesktop({ onLoginClick: handleOpenModal })}
-      {isMobile && Drawer({ onLoginClick: handleOpenModal })}
+      <Toolbar />{" "}
+      {/* Add this to prevent content from going under the AppBar */}
+      <Drawer anchor="right" open={isBurgerMenuOpen} onClose={toggleBurgerMenu}>
+        <Box
+          sx={{ width: 250 }}
+          role="presentation"
+          onClick={toggleBurgerMenu}
+          onKeyDown={toggleBurgerMenu}
+        >
+          <List>
+            {RoutesNav.map((route, index) => (
+              <ListItem key={index} component="a" href={`#${route.toPath}`}>
+                <ListItemText primary={route.routeName} />
+              </ListItem>
+            ))}
+            <ListItem>
+              <Button
+                fullWidth
+                variant="contained"
+                size="large"
+                sx={{
+                  bgcolor: "#212121",
+                  color: "#fff",
+                  "&:hover": {
+                    bgcolor: "#424242",
+                  },
+                }}
+                onClick={() => {
+                  handleOpenModal();
+                  toggleBurgerMenu();
+                }}
+              >
+                <Typography variant="h6">התחברות</Typography>
+              </Button>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
     </Box>
   );
 }
