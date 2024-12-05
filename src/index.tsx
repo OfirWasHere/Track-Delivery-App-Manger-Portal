1c897fc16332db315eceeed8adb9c96b263b0a40
OfirIsHere <Ofirdoron19@gmail.com>
2024-12-05 16:26:04 +0200

    added project to firebase

--- src/index.tsx
+++ src/index.tsx
@@ -23,6 +23,8 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+console.log(firebaseConfig);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
