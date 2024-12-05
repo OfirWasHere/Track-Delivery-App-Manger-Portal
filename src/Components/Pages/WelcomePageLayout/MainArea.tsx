b265c12eacded3474501b40ce0c19332f0d4ea1a
OfirIsHere <Ofirdoron19@gmail.com>
2024-12-05 15:40:47 +0200

    add env and firebase

--- src/Components/Pages/WelcomePageLayout/MainArea.tsx
+++ src/Components/Pages/WelcomePageLayout/MainArea.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "@mui/material";
 import AboutSection from "./Sections/AboutSection";
 import ContactUs from "./Sections/ContactUs";
