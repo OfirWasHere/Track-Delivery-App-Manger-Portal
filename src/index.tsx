3ca9d6cb32bec2157f0096d7f9b424b1b69eb87a
OfirIsHere <Ofirdoron19@gmail.com>
2024-12-15 22:27:11 +0200

    update useFirebase config

--- src/index.tsx
+++ src/index.tsx
@@ -9,22 +9,7 @@ import { Routes } from "./Components/Routes/Routes";
 import { CssBaseline } from "@mui/material";
 import theme from "./Components/theme/theme";
 import { ThemeContextProvider } from "./Components/theme/ThemeContextProvider";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-console.log(firebaseConfig);
-
+import "./Components/Hooks/useFirebase";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
