16e3117f3cc79b1d90092ca85eda4aafc878b686
OfirIsHere <Ofirdoron19@gmail.com>
2025-01-28 12:05:47 +0200

    improve loading times

--- src/Components/Routes/Routes.tsx
+++ src/Components/Routes/Routes.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import PageNotFound from "../Pages/PageNotFound/PageNotFound";
 import Root from "../Pages/Root/Root";
-import Main from "../Pages/ToolpadPages/Main/Main";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import { type Navigation } from "@toolpad/core";
-import Dashboard from "../Pages/ToolpadPages/Dashboard/Dashboard";
 import Login from "../Pages/login/Login";
 import WelcomePage from "../Pages/WelcomePageLayout/WelcomePage";
 import GuardedRoute from "../common/GuardedRoute";
 import { RoutesModel } from "../types/types";
 import Toolpad from "../Pages/ToolpadPages/Toolpad";
+
+// Lazy-loaded components
+const Dashboard = React.lazy(
+  () => import("../Pages/ToolpadPages/Dashboard/Dashboard")
+);
+const Main = React.lazy(() => import("../Pages/ToolpadPages/Main/Main"));
 
 const Routes: RoutesModel[] = [
   {
@@ -29,9 +33,11 @@ const Routes: RoutesModel[] = [
         path: "/Dashboard",
         element: (
           <GuardedRoute>
-            <Toolpad>
-              <Dashboard />
-            </Toolpad>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Toolpad>
+                <Dashboard />
+              </Toolpad>
+            </Suspense>
           </GuardedRoute>
         ),
       },
@@ -39,9 +45,11 @@ const Routes: RoutesModel[] = [
         path: "/Main",
         element: (
           <GuardedRoute>
-            <Toolpad>
-              <Main />
-            </Toolpad>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Toolpad>
+                <Main />
+              </Toolpad>
+            </Suspense>
           </GuardedRoute>
         ),
       },
@@ -64,11 +72,6 @@ const NAVIGATION: Navigation = [
     title: "Main",
     icon: <DashboardIcon />,
   },
-  // {
-  //   segment: "",
-  //   title: "welcomePage",
-  //   icon: <HomeIcon />,
-  // },
 ];
 
 const BRANDING = {
