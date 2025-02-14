ecbb9666b2fede9072e151dc6c036235dbf93533
OfirIsHere <Ofirdoron19@gmail.com>
2025-02-14 19:47:42 +0200

    clean boilerplate code

--- src/Components/common/SideBar.tsx
+++ src/Components/common/SideBar.tsx
@@ -1,16 +1,10 @@
-import {
-  ChevronLeft,
-  ChevronRight,
-  Menu,
-  DoubleArrowRounded,
-} from "@mui/icons-material";
+import { Menu, DoubleArrowRounded } from "@mui/icons-material";
 import {
   Avatar,
   Box,
   Drawer,
   Typography,
   IconButton,
-  AvatarGroup,
   Divider,
 } from "@mui/material";
 import { useState } from "react";
@@ -19,28 +13,24 @@ import FoxLogo from "../../assets/logo.png";
 
 export default function Sidebar() {
   const [open, setOpen] = useState(true);
+  const sideBarWidth = { open: 300, closed: 100 };
 
-  const sideBarOpenWidth = 300;
-  const sideBarClosedWidth = 150;
-
-  const handleSideBarToggle = () => {
-    setOpen(!open);
-  };
+  const handleSideBarToggle = () => setOpen(!open);
 
   return (
     <Drawer
       variant="permanent"
       sx={{
-        width: open ? sideBarOpenWidth : sideBarClosedWidth,
+        width: open ? sideBarWidth.open : sideBarWidth.closed,
         transition: theme.transitions.create("width", {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.standard,
         }),
         "& .MuiDrawer-paper": {
-          width: open ? sideBarOpenWidth : sideBarClosedWidth,
+          width: open ? sideBarWidth.open : sideBarWidth.closed,
           transition: theme.transitions.create("width", {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.standard,
           }),
           overflowX: "hidden",
           bgcolor: theme.palette.grey[900],
@@ -49,53 +39,36 @@ export default function Sidebar() {
         },
       }}
     >
-      <Box width={open ? sideBarOpenWidth : sideBarClosedWidth}>
+      <Box width={open ? sideBarWidth.open : sideBarWidth.closed}>
         <Box sx={{ mx: 2, py: 2 }}>
-          {open ? (
-            <>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "space-around",
-                  gap: 2,
-                }}
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: open ? "space-around" : "center",
+              gap: 2,
+            }}
+          >
+            {open && <Avatar src={FoxLogo} sx={{ width: 40, height: 40 }} />}
+            {open && (
+              <Typography
+                variant="h6"
+                fontWeight="bold"
+                sx={{ letterSpacing: 2 }}
               >
-                <Avatar src={FoxLogo} sx={{ width: 40, height: 40 }} />
-                <Typography
-                  variant="h6"
-                  fontWeight="bold"
-                  sx={{ letterSpacing: 2 }}
-                >
-                  SoftFOX
-                </Typography>
-                <IconButton
-                  onClick={handleSideBarToggle}
-                  sx={{ color: "white" }}
-                >
-                  <DoubleArrowRounded sx={{ fontSize: 32 , transform:"scaleX(-1)"}} />
-                </IconButton>
-              </Box>
-            </>
-          ) : (
-            <>
-              <Box
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Avatar src={FoxLogo} sx={{ width: 40, height: 40 }} />
-                <IconButton
-                  onClick={handleSideBarToggle}
-                  sx={{ color: "white" }}
-                >
-                  <DoubleArrowRounded sx={{ fontSize: 32 }} />
-                </IconButton>
-              </Box>
-            </>
-          )}
+                SoftFOX
+              </Typography>
+            )}
+            <IconButton onClick={handleSideBarToggle} sx={{ color: "white" }}>
+              {open ? (
+                <DoubleArrowRounded
+                  sx={{ fontSize: 32, transform: "scaleX(-1)" }}
+                />
+              ) : (
+                <Menu sx={{ fontSize: 32 }} />
+              )}
+            </IconButton>
+          </Box>
         </Box>
       </Box>
       <Divider sx={{ bgcolor: "rgba(255,255,255,0.1)" }} />
