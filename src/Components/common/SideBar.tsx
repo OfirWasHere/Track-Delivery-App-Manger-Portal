03c2b3b709ab10ab6bf35b7e7e48ce5562ef0107
OfirIsHere <Ofirdoron19@gmail.com>
2025-02-15 13:09:45 +0200

    add sidebar elements and titles

--- src/Components/common/SideBar.tsx
+++ src/Components/common/SideBar.tsx
@@ -1,4 +1,19 @@
-import { Menu, DoubleArrowRounded } from "@mui/icons-material";
+import {
+  Menu,
+  DoubleArrowRounded,
+  BarChart,
+  CalendarToday,
+  CheckCircle,
+  Dashboard,
+  Description,
+  FlashOn,
+  Group,
+  Headset,
+  Inventory,
+  Person,
+  Settings,
+  ChevronRight,
+} from "@mui/icons-material";
 import {
   Avatar,
   Box,
@@ -6,10 +21,52 @@ import {
   Typography,
   IconButton,
   Divider,
+  List,
+  Badge,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Tooltip,
 } from "@mui/material";
 import { useState } from "react";
 import theme from "../theme/theme";
 import FoxLogo from "../../assets/logo.png";
+import React from "react";
+
+const sidebarItems = [
+  {
+    title: "TASKS",
+    items: [
+      { icon: <CheckCircle />, text: "Tasks", badge: 16 },
+      { icon: <FlashOn />, text: "Activities" },
+    ],
+  },
+  {
+    title: "MAIN",
+    items: [
+      { icon: <Dashboard />, text: "Dashboard" },
+      { icon: <CalendarToday />, text: "Schedule" },
+      { icon: <Description />, text: "Note" },
+      { icon: <Inventory />, text: "Products" },
+      { icon: <BarChart />, text: "Report" },
+    ],
+  },
+  {
+    title: "RECORDS",
+    items: [
+      { icon: <Group />, text: "Team" },
+      { icon: <Person />, text: "Clients" },
+    ],
+  },
+  {
+    title: "SETTINGS",
+    items: [
+      { icon: <Settings />, text: "Settings" },
+      { icon: <Headset />, text: "Support" },
+    ],
+  },
+];
 
 export default function Sidebar() {
   const [open, setOpen] = useState(true);
@@ -22,16 +79,10 @@ export default function Sidebar() {
       variant="permanent"
       sx={{
         width: open ? sideBarWidth.open : sideBarWidth.closed,
-        transition: theme.transitions.create("width", {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.standard,
-        }),
+        transition: `0.5s ease-in-out`,
         "& .MuiDrawer-paper": {
           width: open ? sideBarWidth.open : sideBarWidth.closed,
-          transition: theme.transitions.create("width", {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.standard,
-          }),
+          transition: `0.5s ease-in-out`,
           overflowX: "hidden",
           bgcolor: theme.palette.grey[900],
           color: theme.palette.common.white,
@@ -72,6 +123,39 @@ export default function Sidebar() {
         </Box>
       </Box>
       <Divider sx={{ bgcolor: "rgba(255,255,255,0.1)" }} />
+      <List sx={{ px: open ? 2 : 1, py: 1 }}>
+        {sidebarItems.map((section) => (
+          <React.Fragment key={section.title}>
+            <ListItem sx={{ py: 2 }}>
+              <Typography
+                variant="overline"
+                color="rgba(255,255,255,0.7)"
+                fontWeight="bold"
+              >
+                {section.title}
+              </Typography>
+            </ListItem>
+
+            {section.items.map((item) => (
+              <ListItem key={item.text} disablePadding sx={{ mb: 1 }}>
+                <ListItemButton sx={{ display: "flex" }}>
+                  <ListItemIcon
+                    sx={{
+                      color: theme.palette.primary.main,
+                      minWidth: open ? 40 : "auto",
+                      mr: open ? 2 : "auto",
+                      justifyContent: "center",
+                    }}
+                  >
+                    {item.icon}
+                  </ListItemIcon>
+                  <Typography>{item.text}</Typography>
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </React.Fragment>
+        ))}
+      </List>
     </Drawer>
   );
 }
