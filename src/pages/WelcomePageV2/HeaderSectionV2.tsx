00d044a59dae1db8f019caa69eb35e080d838830
OfirIsHere <Ofirdoron19@gmail.com>
2025-03-06 19:04:40 +0200

    update navbar to have height prop

--- src/pages/WelcomePageV2/HeaderSectionV2.tsx
+++ src/pages/WelcomePageV2/HeaderSectionV2.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Typography } from "@mui/material";
 
 function HeaderSection() {
   return (
@@ -7,19 +7,35 @@ function HeaderSection() {
         minHeight: "100vh",
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
+        // alignItems: "center",
+        background: "rgba(16, 144, 203, 0.1)",
       }}
     >
       <Container>
-        <Typography variant="h2" gutterBottom>
-          נהל את צי הנהגים שלך בקלות
-        </Typography>
-        <Typography variant="h5" gutterBottom>
-          נהלו את צי הנהגים שלכם בקלות עם onTrack! עדכנו פרטים, עקבו אחר הובלות,
-          רשמו אירועים, הוציאו דוחות בלחיצת כפתור, וקשרו את הנתונים ישירות לרואה
-          החשבון במהירות. מערכת חכמה, אינטואיטיבית ויעילה שתעשה לכם סדר בניהול
-          העסק.
-        </Typography>
+        <Box sx={{ border: 2, borderColor: "#d32f2f" }}>
+          <Typography variant="h2" gutterBottom>
+            נהל את צי הנהגים שלך בקלות
+          </Typography>
+          <Typography variant="h5" gutterBottom>
+            שדרג את ניהול עסק ההובלות שלך עם המערכת החכמה שלנו – הוספת ועריכת
+            משלוחים, ניהול נהגים ורכבים, מעקב אחר סטטוס הובלות, תיעוד מפורט ועוד
+            – הכל במקום אחד, בצורה פשוטה, חכמה ויעילה.
+          </Typography>
+          <Box display="flex" gap="20px" sx={{ mt: 2 }}>
+            <Button
+              variant="contained"
+              color="info"
+              sx={{ borderRadius: "8px" }}
+            >
+              <Typography sx={{ fontSize: 24, padding: "4px" }}>
+                הרשמה
+              </Typography>
+            </Button>
+            <Button variant="outlined" sx={{ borderRadius: "8px" }}>
+              <Typography sx={{ fontSize: 24 }}>פרטים נוספים</Typography>
+            </Button>
+          </Box>
+        </Box>
       </Container>
     </Box>
   );
