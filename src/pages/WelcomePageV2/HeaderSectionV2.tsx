02f84891161f987d1dfdd107481df88adcea7cde
OfirIsHere <Ofirdoron19@gmail.com>
2025-03-06 20:17:03 +0200

    add background

--- src/pages/WelcomePageV2/HeaderSectionV2.tsx
+++ src/pages/WelcomePageV2/HeaderSectionV2.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Container, Typography } from "@mui/material";
+import placeholderImage1 from "../../assets/placeholderimage.jpg";
 
 function HeaderSection() {
   return (
@@ -7,36 +8,54 @@ function HeaderSection() {
         minHeight: "100vh",
         display: "flex",
         justifyContent: "center",
-        // alignItems: "center",
         background: "rgba(16, 144, 203, 0.1)",
       }}
     >
-      <Container>
-        <Box sx={{ border: 2, borderColor: "#d32f2f" }}>
-          <Typography variant="h2" gutterBottom>
-            נהל את צי הנהגים שלך בקלות
-          </Typography>
-          <Typography variant="h5" gutterBottom>
-            שדרג את ניהול עסק ההובלות שלך עם המערכת החכמה שלנו – הוספת ועריכת
-            משלוחים, ניהול נהגים ורכבים, מעקב אחר סטטוס הובלות, תיעוד מפורט ועוד
-            – הכל במקום אחד, בצורה פשוטה, חכמה ויעילה.
-          </Typography>
-          <Box display="flex" gap="20px" sx={{ mt: 2 }}>
-            <Button
-              variant="contained"
-              color="info"
-              sx={{ borderRadius: "8px" }}
+      <Box width="100%">
+        <Box
+          sx={{
+            background: `url(${placeholderImage1})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "top",
+            height: "auto",
+            py: 20,
+          }}
+        >
+          <Container>
+            <Typography
+              variant="h2"
+              color="#fff"
+              fontWeight="bold"
+              gutterBottom
             >
-              <Typography sx={{ fontSize: 24, padding: "4px" }}>
-                הרשמה
-              </Typography>
-            </Button>
-            <Button variant="outlined" sx={{ borderRadius: "8px" }}>
-              <Typography sx={{ fontSize: 24 }}>פרטים נוספים</Typography>
-            </Button>
-          </Box>
+              נהל את צי הנהגים שלך בקלות
+            </Typography>
+
+            <Typography
+              variant="h5"
+              color="#fff"
+              fontWeight="bold"
+              gutterBottom
+            >
+              שדרג את ניהול עסק ההובלות שלך עם המערכת החכמה שלנו – הוספת ועריכת
+              משלוחים, ניהול נהגים ורכבים, מעקב אחר סטטוס הובלות, תיעוד מפורט
+              ועוד – הכל במקום אחד, בצורה פשוטה, חכמה ויעילה.
+            </Typography>
+
+            <Box display="flex" gap="20px" sx={{ mt: 2 }}>
+              <Button variant="contained" sx={{ borderRadius: "8px" }}>
+                <Typography sx={{ fontSize: 24, padding: "4px" }}>
+                  הרשמה
+                </Typography>
+              </Button>
+              <Button variant="outlined" sx={{ borderRadius: "8px" }}>
+                <Typography sx={{ fontSize: 24 }}>פרטים נוספים</Typography>
+              </Button>
+            </Box>
+          </Container>
         </Box>
-      </Container>
+      </Box>
     </Box>
   );
 }
